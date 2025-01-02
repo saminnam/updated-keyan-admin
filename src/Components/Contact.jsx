40d6f20f3ee77c7bf65ad34e17b9f1220c1df74c
Your Name <your.email@example.com>
2025-01-02 10:57:23 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,20 +2,21 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { AgGridReact } from "ag-grid-react";
-import { RiCodeView, RiDeleteBinLine } from "react-icons/ri";
+import { RiDeleteBinLine } from "react-icons/ri";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { setContacts } from "../Redux/Actions/ContactsActions";
 import { setError } from "../Redux/Actions/CommonActions";
 import { API_BASE_URL } from "./Api";
+import { setSelectedId, setPopUpVisible } from "../Redux/Actions/CommonActions";
 import Loader from "./Loader";
+import DeletePopUp from "../Modals/DeletePopUp";
 
 const Contact = () => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const { contacts } = useSelector((state) => state.contactsInfo);
-  const { error } = useSelector((state) => state.commonInfo);
-  const [selectedMessage, setSelectedMessage] = useState(null);
+  const { error, selectedId } = useSelector((state) => state.commonInfo);
 
   useEffect(() => {
     fetchContacts();
@@ -35,31 +36,29 @@ const Contact = () => {
         console.error("Error fetching data:", error.message);
       });
   };
-
-  const handleViewClick = (contact) => {
-    axios
-      .put(`${API_BASE_URL}contacts/${contact._id}/read`)
-      .then(() => {
-        const updatedContacts = contacts.map((msg) =>
-          msg._id === contact._id ? { ...msg, status: "Read" } : msg
-        );
-        dispatch(setContacts(updatedContacts));
-        setSelectedMessage({ ...contact, status: "Read" });
-      })
-      .catch((error) => {
-        setError("Error marking contact as read", error.message);
-      });
+  const confirmDelete = (id) => {
+    dispatch(setSelectedId(id));
+    dispatch(setPopUpVisible(true));
   };
 
-  const handleDelete = (id) => {
-    axios
-      .delete(`${API_BASE_URL}contacts/${id}`)
-      .then(() => {
-        fetchContacts();
-      })
-      .catch((error) => {
-        setError("Failed to delete. Please try again.", error.message);
-      });
+  const handleDelete = () => {
+    if (selectedId) {
+      axios
+        .delete(`${API_BASE_URL}contacts/${selectedId}`)
+        .then(() => {
+          fetchContacts();
+          dispatch(setPopUpVisible(false));
+          setSelectedId(null);
+        })
+        .catch((error) => {
+          setError("Failed to delete. Please try again.", error.message);
+        });
+    }
+  };
+
+  const cancelDelete = () => {
+    dispatch(setPopUpVisible(false));
+    setSelectedId(null);
   };
 
   const columnDefs = [
@@ -72,25 +71,27 @@ const Contact = () => {
     },
     { headerName: "Email", field: "email", sortable: true, filter: true },
     { headerName: "Subject", field: "subject", sortable: true, filter: true },
-    { headerName: "Message", field: "message", sortable: true, filter: true , width: 250},
+    {
+      headerName: "Message",
+      field: "message",
+      sortable: true,
+      filter: true,
+      width: 250,
+    },
     { headerName: "Status", field: "status", sortable: true, filter: true },
     {
       headerName: "Actions",
       field: "actions",
       pinned: "right",
       cellRenderer: (params) => (
-        <div className="flex space-x-2 mt-3">
-          <RiCodeView
-            className="text-xl cursor-pointer"
-            onClick={() => handleViewClick(params.data)}
-          />
+        <div className="flex items-center justify-center mt-3">
           <RiDeleteBinLine
             className="text-xl cursor-pointer"
-            onClick={() => handleDelete(params.data._id)}
+            onClick={() => confirmDelete(params.data._id)}
           />
         </div>
       ),
-      width: 130,
+      width: 100,
     },
   ];
 
@@ -105,7 +106,9 @@ const Contact = () => {
           <Loader />
         </div>
       ) : (
-        <div className="ag-theme-alpine mt-2" style={{ height: 400, width: "100%" }}>
+        <div
+          className="ag-theme-alpine mt-2"
+        >
           <AgGridReact
             rowData={contacts}
             columnDefs={columnDefs}
@@ -117,34 +120,7 @@ const Contact = () => {
           />
         </div>
       )}
-      {selectedMessage && (
-        <div className="p-5 border border-gray-300 rounded-lg bg-gray-100 relative">
-          <button
-            onClick={() => setSelectedMessage(null)}
-            className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-          >
-            &#x2715;
-          </button>
-          <h3 className="text-3xl font-bold mb-4 font-serif">
-            Message Details
-          </h3>
-          <p>
-            <strong>Name:</strong> {selectedMessage.name}
-          </p>
-          <p>
-            <strong>Email:</strong> {selectedMessage.email}
-          </p>
-          <p>
-            <strong>Subject:</strong> {selectedMessage.subject}
-          </p>
-          <p>
-            <strong>Message:</strong> {selectedMessage.message}
-          </p>
-          <p>
-            <strong>Status:</strong> {selectedMessage.status}
-          </p>
-        </div>
-      )}
+      <DeletePopUp onConfirmDelete={handleDelete} onCancel={cancelDelete} />
     </section>
   );
 };
